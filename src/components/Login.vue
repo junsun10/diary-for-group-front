e7629bd6afee10f0b9dccf2ff089016ad1494058
Junsun Lee <junsun1014@naver.com>
2024-02-28 00:31:32 +0900

    feat: add password hashing

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,23 +1,23 @@
 <template>
   <div>
     <h1>로그인</h1>
-    <form @submit.prevent="login">
-      <input type="text" v-model="name" placeholder="Username" required>
-      <input type="password" v-model="password" placeholder="Password" required>
-      <button type="submit">로그인</button>
+    <form @submit.prevent="login" class="login-form">
+      <input type="text" v-model="name" placeholder="Username" class="form-control" required>
+      <input type="password" v-model="password" placeholder="Password" class="form-control" required>
+      <button type="submit" class="btn-submit">로그인</button>
     </form>
-    <p v-if="errorMessage" style="color: red;">{{ errorMessage }}</p>
+    <p v-if="errorMessage" class="error-message">{{ errorMessage }}</p>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
+import CryptoJS from 'crypto-js';
 
 export default {
   name: 'LoginPage',
   data() {
     return {
-      id: '',
       name: '',
       password: '',
       errorMessage: ''
@@ -26,9 +26,11 @@ export default {
   methods: {
     async login() {
       try {
+        // 비밀번호 암호화
+        const hashedPassword = CryptoJS.SHA256(this.password).toString(CryptoJS.enc.Hex);
         const response = await axios.post('http://localhost:8080/members/login', {
           name: this.name,
-          password: this.password
+          password: hashedPassword
         },
         {
           withCredentials: true
@@ -40,15 +42,46 @@ export default {
           this.$store.commit('login', {userId: response.data.id, userName: response.data.name});
           // 메인 페이지로 이동
           this.$router.push('/main');
-        } else {
-          // 로그인 실패
-          this.errorMessage = 'Invalid username or password';
         }
       } catch (error) {
-        console.error('Error during login:', error);
-        this.errorMessage = 'An error occurred during login';
+        console.error('Error logging in:', error.response.data.message);
+        this.errorMessage = error.response.data.message;
       }
     }
   }
 };
 </script>
+
+<style scoped>
+.login-form {
+  max-width: 400px;
+  margin: 0 auto;
+}
+
+.form-control {
+  width: 100%;
+  padding: 10px;
+  font-size: 16px;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+  margin-bottom: 10px;
+}
+
+.btn-submit {
+  padding: 10px 20px;
+  font-size: 14px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.btn-submit:hover {
+  background-color: #a8aaac;
+  color: white;
+}
+
+.error-message {
+  color: red;
+  font-size: 14px;
+}
+</style>
--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,57 +1,108 @@
 <template>
-    <div>
-      <h2>회원가입</h2>
-      <form @submit.prevent="submitForm">
-        <div>
-          <label for="name">아이디:</label>
-          <input type="text" id="username" v-model="formData.name" required>
-        </div>
-        <div>
-          <label for="password">비밀번호:</label>
-          <input type="password" id="password" v-model="formData.password" required>
-        </div>
-        <div>
-          <label for="email">이메일:</label>
-          <input type="email" id="email" v-model="formData.email" required>
-        </div>
-        <button type="submit">가입하기</button>
-      </form>
-    </div>
-  </template>
-  
-  <script>
-  import axios from 'axios';
-  
-  export default {
-    data() {
-      return {
-        formData: {
-          name: '',
-          password: '',
-          email: ''
-        }
-      };
-    },
-    methods: {
-      async submitForm() {
-        try {
-          const response = await axios.post('http://localhost:8080/members/new',
-          this.formData,
-          {
-            withCredentials: true
-          });
+  <div>
+    <h2>회원가입</h2>
+    <form @submit.prevent="submitForm" class="signup-form">
+      <div class="form-group">
+        <input type="text" v-model="formData.name" placeholder="Username" class="form-control" required>
+      </div>
+      <div class="form-group">
+        <input type="password" v-model="formData.password" placeholder="Password" class="form-control" required>
+        <p v-if="!isPasswordValid" class="error-message">비밀번호는 6~20자로 만들어주세요</p>
+      </div>
+      <div class="form-group">
+        <input type="email" v-model="formData.email" placeholder="Email" class="form-control" required>
+      </div>
+      <button type="submit" class="btn-submit">가입하기</button>
+    </form>
+    <p v-if="errorMessage" class="error-message">{{ errorMessage }}</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import CryptoJS from 'crypto-js';
+
+export default {
+  data() {
+    return {
+      formData: {
+        name: '',
+        password: '',
+        email: '',
+      },
+      errorMessage: ''
+    };
+  },
+  computed: {
+    isPasswordValid() {
+      return this.formData.password.length >= 6 && this.formData.password.length <= 20;
+    },    
+  },
+  methods: {
+    async submitForm() {
+      try {
+        // 비밀번호 암호화
+        const hashedPassword = CryptoJS.SHA256(this.formData.password).toString(CryptoJS.enc.Hex);
+        const response = await axios.post('http://localhost:8080/members/new',
+        {
+          name: this.formData.name,
+          password: hashedPassword,
+          email: this.formData.email
+        },
+        {
+          withCredentials: true
+        });
+        if (response.status === 201) {
+          // 회원가입 성공
+          // 로그인 페이지로 이동
           console.log('회원가입 성공:', response.data);
           this.$router.push('/login');
-        } catch (error) {
-          console.error('회원가입 실패:', error.response.data.message);
-          // 회원가입 실패 처리
         }
+      } catch (error) {
+        console.error('회원가입 실패:', error);
+        this.errorMessage = error.response.data.message;
       }
     }
-  };
-  </script>
+  }
+};
+</script>
   
-  <style scoped>
-  /* 스타일 작성 */
-  </style>
-  
+<style scoped>
+.signup-form {
+  max-width: 400px;
+  margin: 0 auto;
+}
+.form-group {
+  margin-bottom: 20px;
+}
+
+.form-control {
+  width: 100%;
+  padding: 10px;
+  font-size: 16px;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+}
+
+.error-message {
+  color: red;
+  font-size: 14px;
+}
+
+.btn-submit {
+  padding: 10px 20px;
+  font-size: 14px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.btn-submit:hover {
+  background-color: #a8aaac;
+  color: white;
+}
+.error-message {
+  color: red;
+  font-size: 14px;
+}
+</style>
